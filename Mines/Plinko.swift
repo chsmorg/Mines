389f3253549e3bd6efe71bc8a5f342f17a1297ab
chsmorg <58636766+chsmorg@users.noreply.github.com>
2021-12-19 15:35:15 -0600

    fixed size of boxes on pinko

--- Mines/Plinko.swift
+++ Mines/Plinko.swift
@@ -26,18 +26,24 @@ struct Plinko: View {
                 ForEach(0..<(balls+Int(rowNum)), id: \.self){ ballNum in
                     if((balls + rowNum) % 2 == 0)
                     {
-                        CircleEven(circle_Width: Double(ballSize/Int(states.rows)), circle_Height: Double(ballSize/Int(states.rows)), postion_Width: geometry.size.width, balls: Double(balls+rowNum), ballNum: Double(ballNum), rows: states.rows+1.5, rowNum: Double(rowNum))
+                        CircleEven(circle_Width: Double(ballSize/Int(states.rows)), circle_Height: Double(ballSize/Int(states.rows)), postion_Width: geometry.size.width, balls: Double(balls+rowNum), ballNum: Double(ballNum), rows: Int(states.rows), rowNum: Double(rowNum))
                     }
                     
                     else{
-                        CircleOdd(circle_Width: Double(ballSize/Int(states.rows)), circle_Height: Double(ballSize/Int(states.rows)), postion_Width: geometry.size.width, balls: Double(balls+rowNum), ballNum: Double(ballNum), rows: states.rows+1.5, rowNum: Double(rowNum))
+                        CircleOdd(circle_Width: Double(ballSize/Int(states.rows)), circle_Height: Double(ballSize/Int(states.rows)), postion_Width: geometry.size.width, balls: Double(balls+rowNum), ballNum: Double(ballNum), rows: Int(states.rows), rowNum: Double(rowNum))
                     }
                     
                 }
                 
             }
             ForEach(0..<Int(states.rows)+1, id: \.self){ box in
-                Box(postion_Width: geometry.size.width, boxSize: 140, boxs: Double(box), rows: states.rows)
+                if(Int(states.rows) + 1 % 2 == 0){
+                    EvenBox(postion_Width: geometry.size.width, boxSize: 140.0, boxs: Double(box), rows: Int(states.rows))
+                }
+                else{
+                    EvenBox(postion_Width: geometry.size.width, boxSize: 140.0, boxs: Double(box), rows: Int(states.rows))
+                }
+                
             }
             
             
@@ -55,13 +61,13 @@ struct CircleEven: View {
     var postion_Width: Double
     var balls: Double
     var ballNum: Double
-    var rows: Double
+    var rows: Int
     var rowNum: Double
     var body: some View{
         
         
         //  ((geometry.size.width/Double(rows) * Double((balls+rowNum)/2)) + (geometry.size.width/Double(rows)) * 0.5)
-        Circle().fill(colorScheme == .dark ? Color.white : Color.black).frame(width: circle_Width, height: circle_Height).position(x: (postion_Width * 0.5 - ((postion_Width/rows) * (balls)/2 - postion_Width/rows * 0.5)) + (postion_Width/rows * ballNum)  ,y: postion_Width/rows * 2.0 + postion_Width/rows * rowNum)
+        Circle().fill(colorScheme == .dark ? Color.white : Color.black).frame(width: circle_Width, height: circle_Height).position(x: (postion_Width * 0.5 - ((postion_Width/(Double(rows)+1.5)) * (balls)/2 - postion_Width/(Double(rows)+1.5) * 0.5)) + (postion_Width/(Double(rows)+1.5) * ballNum)  ,y: postion_Width/(Double(rows)+1.5) * 2.0 + postion_Width/(Double(rows)+1.5) * rowNum)
     }
 }
 
@@ -72,16 +78,54 @@ struct CircleOdd: View {
     var postion_Width: Double
     var balls: Double
     var ballNum: Double
-    var rows: Double
+    var rows: Int
     var rowNum: Double
     var body: some View{
         
         
         
-        Circle().fill(colorScheme == .dark ? Color.white : Color.black).frame(width: circle_Width, height: circle_Height).position(x: (postion_Width * 0.5 - (postion_Width/rows) * (balls-1)/2) + (postion_Width/rows * ballNum)  ,y: postion_Width/rows * 2.0 + postion_Width/rows * rowNum)
+        Circle().fill(colorScheme == .dark ? Color.white : Color.black).frame(width: circle_Width, height: circle_Height).position(x: (postion_Width * 0.5 - (postion_Width/(Double(rows)+1.5)) * (balls-1)/2) + (postion_Width/(Double(rows)+1.5) * ballNum)  ,y: postion_Width/(Double(rows)+1.5) * 2.0 + postion_Width/(Double(rows)+1.5) * rowNum)
     }
 }
-struct Box: View {
+struct EvenBox: View {
+    var postion_Width: Double
+    var boxSize: Double
+    var boxs: Double
+    var rows: Int
+    var body: some View{
+        
+        if(boxs < (Double(rows)) / 2 + ((Double(rows)) * 0.15) && boxs > Double(rows)/2 - ((Double(rows)) * 0.15)){
+            RoundedRectangle(cornerRadius: 100.0/(Double(rows)+1.5))
+                .fill(Color.yellow)
+                .frame(width: boxSize/((Double(rows)+1.5)/2), height: boxSize/((Double(rows)+1.5)/2))
+                .position(x:  postion_Width * 0.5 - (postion_Width/((Double(rows)+1.5)+1.7)) * ((Double(rows)+1.5)/2) + (postion_Width/(Double(rows)+1.5) * boxs), y: postion_Width/(Double(rows)+1.5) * 2.0 + postion_Width/(Double(rows)+1.5) * ((Double(rows)+1.5)-1.8))
+            
+        }
+        else if(boxs < (Double(rows))/2 + (Double(rows)) * 0.35 && boxs > (Double(rows))/2 - (Double(rows)) * 0.35)
+        {
+           
+            RoundedRectangle(cornerRadius: 100.0/(Double(rows)+1.5))
+                .fill(Color.orange)
+                .frame(width: boxSize/((Double(rows)+1.5)/2), height: boxSize/((Double(rows)+1.5)/2))
+                .position(x:  postion_Width * 0.5 - (postion_Width/((Double(rows)+1.5)+1.7)) * ((Double(rows)+1.5)/2) + (postion_Width/(Double(rows)+1.5) * boxs), y: postion_Width/(Double(rows)+1.5) * 2.0 + postion_Width/(Double(rows)+1.5) * ((Double(rows)+1.5)-1.8))
+            
+        }
+        else{
+            
+            RoundedRectangle(cornerRadius: 100.0/(Double(rows)+1.5))
+                .fill(Color.red)
+                .frame(width: boxSize/((Double(rows)+1.5)/2), height: boxSize/((Double(rows)+1.5)/2))
+                .position(x:  postion_Width * 0.5 - (postion_Width/((Double(rows)+1.5)+1.7)) * ((Double(rows)+1.5)/2) + (postion_Width/(Double(rows)+1.5) * boxs), y: postion_Width/(Double(rows)+1.5) * 2.0 + postion_Width/(Double(rows)+1.5) * ((Double(rows)+1.5)-1.8))
+            
+        }
+        
+        //(postion_Width * 0.5 - ((postion_Width/rows) * (balls)/2 - postion_Width/rows * 0.5)) + (postion_Width/rows * ballNum)
+        
+    }
+    
+}
+
+struct OddBox: View {
     var postion_Width: Double
     var boxSize: Double
     var boxs: Double
@@ -106,4 +150,5 @@ struct Box: View {
     }
     
 }
+
 
