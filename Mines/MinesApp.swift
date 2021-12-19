d025a9350bef4ba782278f13658e417c6cb710ac
chsmorg <58636766+chsmorg@users.noreply.github.com>
2021-12-19 04:12:18 -0600

    Initial Commit

---
+++ Mines/MinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesApp.swift
+//  Mines
+//
+//  Created by chase morgan on 12/19/21.
+//
+
+import SwiftUI
+
+@main
+struct MinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
